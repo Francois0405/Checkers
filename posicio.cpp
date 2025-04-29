251fde8663b18c434f3386a349367bff059a83e5
Jennifer Martínez <jenniferdenisse.martinez@autonoma.cat>
2025-04-29 11:28:04 +0200

    funcions de posicio.cpp

--- posicio.cpp
+++ posicio.cpp
@@ -3,13 +3,57 @@
 /**
 * FITXER posicio.cpp
 * AUTOR Jennifer Martínez | François Liraud
-* DATA 19/03/2025
+* DATA 29/03/2025
 * VERSIO 1.0
 * Aquest fitxer conté les funcions de la classe Posicio
 */
 
 const int N_FILES = 8;
 const int N_COLUMNES = 8;
+
+Posicio::Posicio() : m_fila(0), m_columna('a') {}
+
+Posicio::Posicio(const string& posicio)
+{
+	m_columna = posicio[0];
+	m_fila = posicio[1] - '0';
+}
+
+Posicio::Posicio(int fila, int columna)
+{
+	m_fila = fila;
+	m_columna = 'a' + columna;
+}
+
+int Posicio::getFila() const
+{
+	return m_fila;
+}
+
+int Posicio::getColumna() const
+{
+	return m_columna - 'a';
+}
+
+void Posicio::setFila(int fila)
+{
+	m_fila = fila;
+}
+
+void Posicio::setColumna(int columna)
+{
+	m_columna = 'a' + columna;
+}
+
+bool Posicio::operator==(const Posicio& posicio) const
+{
+	return (m_fila == posicio.m_fila && m_columna == posicio.m_columna);
+}
+
+bool Posicio::operator!=(const Posicio& posicio) const
+{
+
+}
 
 void Posicio::stringToPosicio(const string& posicio, int& fila, int& columna)
 {
