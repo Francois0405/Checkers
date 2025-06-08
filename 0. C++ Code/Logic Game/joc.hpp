91dde4049b34b7d25f36c3da1c2f159332abd2f7
François Liraud <fliraud.edu@gmail.com>
2025-06-08 16:00:52 +0200

    Fusion parte grafica + codigo

---
+++ 0. C++ Code/Logic Game/joc.hpp
@@ -0,0 +1,24 @@
+#ifndef JOC_H
+#define JOC_H
+
+#include <stdio.h>
+#include "info_joc.hpp"
+
+using namespace std;
+
+
+class Joc 
+{
+
+public:
+    Joc(): m_fila(0), m_columna(0) {};
+    
+    bool actualitza(int mousePosX, int mousePosY, bool mouseStatus);
+    
+    
+private:
+    int m_fila, m_columna;
+    Tauler m_tauler;
+};
+
+#endif 
